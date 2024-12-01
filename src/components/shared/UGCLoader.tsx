94df759c38b27c022ee8c849cedae37ec918297d
imgopaal <gopaldax@gmail.com>
2024-12-01 19:29:16 +0500

    feat: dynamic loader position with respect to side nav

--- src/components/shared/UGCLoader.tsx
+++ src/components/shared/UGCLoader.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
 import imgSrc from '../../assets/animation/Loading_Animation-Transparent.gif'
 
 const UGCLoader: React.FC = () => {
-	const location = useLocation()
-	const isRootRoute = location.pathname === '/' || location.pathname === '/sign-in' || location.pathname === '/sign-up'   
+	const [hasSidebar, setHasSidebar] = useState(false)
+
+	useEffect(() => {
+		// Check if a sidebar exists in the DOM
+		const sidebarElement = document.querySelector('.side-nav')
+		setHasSidebar(!!sidebarElement)
+	}, [])
 
 	return (
-		<div className={`fixed inset-0 flex items-center justify-center ${isRootRoute ? 'left-0' : 'left-64'}`}>
+		<div className={`fixed inset-0 flex items-center justify-center ${hasSidebar ? 'left-64' : 'left-0'}`}>
 			<img src={imgSrc} alt="Loading animation" className="h-64 opacity-20" />
 		</div>
 	)
